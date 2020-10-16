19dc28f6e5ea61485afea5bae16dd30fd63fceaa
DESKTOP-7UQJTRQ\Quy_cut3 <phamhoangquy99vn@gmail.com>
2020-10-16 10:53:49 +0700

    Add pages support-client

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+$(document).ready(function() {
+    toggleSupport();
+});
+
 // header
 $('.header_btn').click(function() {
     $(this).toggleClass("click");
@@ -9,3 +13,28 @@ $('.overlay').click(function() {
     $('.menu_mobile').toggleClass("show");
     $('.overlay').toggleClass("show");
 });
+
+// toggle support
+function toggleSupport() {
+    $(".toggle-item > .title").click(function(e) {
+        e.preventDefault();
+        if (!$(this)
+            .parent()
+            .hasClass("active")
+        ) {
+            $(".toggle-item .article").slideUp();
+            $(this)
+                .next()
+                .slideToggle();
+            $(".toggle-item").removeClass("active");
+            $(this)
+                .parent()
+                .addClass("active");
+        } else {
+            $(this)
+                .next()
+                .slideToggle();
+            $(".toggle-item").removeClass("active");
+        }
+    });
+}
