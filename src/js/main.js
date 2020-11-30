0e2c9a907692739d38c8a8752d44ca5691327eec
DESKTOP-7UQJTRQ\Quy_cut3 <phamhoangquy99vn@gmail.com>
2020-11-30 09:50:22 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -301,7 +301,6 @@ function swiperInit() {
                     direction: "vertical"
                 }
             },
-            centeredSlides: true,
             direction: "vertical",
             watchSlidesVisibility: true,
             watchSlidesProgress: true,
